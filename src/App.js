e22579fa1acf4bee3b10b16adc5ab473df114bd3
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-04-14 17:40:47 +0530

    Redirect Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
@@ -12,6 +12,8 @@ function App() {
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/posts" component={Posts} />
+        <Route path="/home" />
+        <Redirect to="/" />
         <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
