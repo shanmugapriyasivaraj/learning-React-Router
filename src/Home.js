a1ab56c6b04a21dbe27dd44e27c67ccbe268029a
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-04-14 19:45:56 +0530

    History

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,17 @@
-function Home() {
-  return <p>Home Page..!</p>;
+function Home({ history }) {
+  return (
+    <>
+      <p>Home Page..!</p>
+      <button onClick={history.goBack}>Go Back</button>
+      <button onClick={history.goForward}>Go Forward</button>
+      <button onClick={() => history.push("/posts")}>
+        Go to Posts Page - Push
+      </button>
+      <button onClick={() => history.replace("/posts")}>
+        Go to Posts Page - Replace
+      </button>
+    </>
+  );
 }
 
 export default Home;
